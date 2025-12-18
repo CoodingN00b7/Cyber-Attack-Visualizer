72a5c8f76ed5a7243193e6aa280f772db1664a96
Fardeen Akmal <fardeenakmal123@gmail.com>
2025-12-18 19:18:49 +0530

    Update main.jsx

--- cyber-visualizer/src/main.jsx
+++ cyber-visualizer/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// Use HashRouter for better compatibility with GitHub Pages deployment
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
